60a5c7e47f4135775c68ebf54816bac73fa8443b
Denis Gordeev <dgordeev@servicetitan.com>
2020-08-31 23:02:01 +0300

    Build to ES

--- host/packages/application/src/app.tsx
+++ host/packages/application/src/app.tsx
@@ -8,11 +8,11 @@ import { IWidget } from '@sandboxes/feature-abstractions';
 import './app.css';
 
 const Widget = React.lazy<IWidget>(async () => {
-    const bundle: { default: { Widget: IWidget } } = await importShim(
+    const { Widget } = await importShim(
         'https://unpkg.com/@sandboxes/feature/dist/bundle/index.js'
     );
 
-    return Promise.resolve({ default: bundle.default.Widget });
+    return Promise.resolve({ default: Widget });
 });
 
 export const App: React.FC = () => (
