0151cbe7c1d360cdb9eb1977605f2ae73b4d5d3b
Denis Gordeev <dgordeev@servicetitan.com>
2020-08-31 21:43:33 +0300

    Cleanup

--- feature/packages/core/webpack.prod.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mode: 'production',
-    entry: './dist/index.js',
-    output: {
-        libraryTarget: 'commonjs-module',
-        filename: './bundle/index.js',
-    },
-};
