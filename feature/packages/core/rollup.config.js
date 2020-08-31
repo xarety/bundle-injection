1c862791e25ffc46e247ff80662e0564c704ec30
Denis Gordeev <dgordeev@servicetitan.com>
2020-08-31 22:49:06 +0300

    CSS modules

--- feature/packages/core/rollup.config.js
+++ feature/packages/core/rollup.config.js
@@ -2,6 +2,7 @@ const commonjs = require('@rollup/plugin-commonjs');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const globals = require('rollup-plugin-node-globals');
 const builtins = require('rollup-plugin-node-builtins');
+const postcss = require('rollup-plugin-postcss');
 
 module.exports = {
     input: './dist/index.js',
@@ -9,5 +10,15 @@ module.exports = {
         dir: 'dist/bundle',
         format: 'es',
     },
-    plugins: [commonjs(), nodeResolve(), globals(), builtins()],
+    plugins: [
+        commonjs(),
+        nodeResolve(),
+        globals(),
+        builtins(),
+        postcss({
+            sourceMap: true,
+            extract: true,
+            minimize: true,
+        }),
+    ],
 };
--- feature/packages/core/src/widget.tsx
+++ feature/packages/core/src/widget.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { IWidget } from '@sandboxes/feature-abstractions';
 
+import Styles from './widget.module.css';
+
 export const Widget: IWidget = ({ title, message }) => (
     <div>
         <div>Injected component</div>
-        <h1>{title}</h1>
+        <h1 className={Styles.header}>{title}</h1>
         <div>{message}</div>
     </div>
 );
